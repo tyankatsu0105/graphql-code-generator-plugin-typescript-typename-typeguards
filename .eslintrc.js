a833c78af16fabef0a5bbc27708daca1fd7a090c
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-08-13 21:11:57 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+
+    "plugin:import/typescript",
+    "plugin:import/recommended",
+
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "simple-import-sort",
+    "sort-keys-fix",
+    "sort-destructure-keys",
+  ],
+  root: true,
+  rules: {
+    "sort-keys-fix/sort-keys-fix": "error",
+    "sort-destructure-keys/sort-destructure-keys": "warn",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "@typescript-eslint/sort-type-constituents": "error",
+  },
+};
